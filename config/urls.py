c188ace2aa4f1da9a5751c66153787944112742c
Sardorbek-SSA <sardorbekabdurakhmonov52@gmail.com>
2026-01-15 23:20:40 +0500

    changes

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from shop.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
+    path('', home, name='home'),
 ]
 
 if settings.DEBUG:
